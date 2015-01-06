b24f14a0276858b584f72bbb8ee01c546506f339
Ming Li <mingli.sgg@gmail.com>
2015-01-06 21:55:49 +0100

    the creation of the homepage

---
+++ js/branding.js
@@ -0,0 +1,7 @@
+function ucbheader(){
+	document.write('<img src="imgs/ucb.png" border="0" usemap="#UCBMap" /><map name="UCBMap" id="UCBMap"><area shape="rect" coords="11,3,168,27" href="http://www.uni-heidelberg.de" target="_blank" /><area shape="rect" coords="805,7,845,25" href="index.html"  /><area shape="rect" coords="865,7,905,25" href="index_cn.html" alt="CU Search"  /></map>');
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.colorado.edu"><img src="imgs/wordmark.png" alt="University of Colorado at Boulder" /></a>');	
+}
